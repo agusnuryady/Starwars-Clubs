b17f5c0ffb5acc701625f2f1fb0147ed4dbe0478
Rahmat Agus Nuryady 59 <agusxlciti@gmail.com>
2022-01-19 06:17:51 +0700

    add notification

--- App.js
+++ App.js
@@ -2,10 +2,17 @@ import React from 'react'
 import { StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { colors } from './src/configs'
+import PushNotification from 'react-native-push-notification';
 
+import { colors } from './src/configs'
 import { store, persistor } from './/src/redux'
 import AppNavigation from './src/routes'
+
+PushNotification.createChannel({
+    channelId: "channel-id-1",
+    channelName: 'Starwars Club Mobile Apps',
+    channelDescription: 'Starwars Club Mobile Apps Push Channel'
+})
 
 const App = () => {
     return (
--- src/containers/ExplorePage.js
+++ src/containers/ExplorePage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
 import Geolocation  from '@react-native-community/geolocation'
 import { useDispatch, useSelector } from 'react-redux'
+import PushNotification from 'react-native-push-notification'
 
 import { AllianceListComponent, CreateAllianceModal, HeaderComponent } from '../components'
 import ContextProvider from '../context/CustomContext'
@@ -11,6 +12,7 @@ import PersistActions from '../redux/PersistRedux'
 const ExplorePage = ({route, navigation}) => {
     const dispatch = useDispatch()
     const persistState = useSelector(state => state.persist)
+    const allianceState = useSelector(state => state.alliance)
     const [modalVisible, setModalVisible] = useState(false)
 
     useEffect(() => {
@@ -23,6 +25,17 @@ const ExplorePage = ({route, navigation}) => {
             dispatch(PersistActions.locationRequest(data))
         })
     }, [])
+
+    useEffect(() => {
+        const myReq = allianceState.troops?.filter(item => item.email === persistState.authUser.email && item.status === 'requested')
+        if (myReq.length > 0) {
+            PushNotification.localNotification({
+                channelId: "channel-id-1",
+                title: 'Alliance Invitation',
+                message: 'You Have Invitation From Alliance!'
+            })
+        } 
+    }, [allianceState, persistState])
 
     const contextValues = () => {
         return {
